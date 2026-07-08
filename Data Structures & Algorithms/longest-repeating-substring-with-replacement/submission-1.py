f1725c9c1bcd27f45234a568f4733b389bd47bac
raymond-sangalang <59466702+raymond-sangalang@users.noreply.github.com>
2026-07-07 18:41:37 -0700

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        hmap = {}          # counts freq of characters
+        left = right = 0   # sides/boundaries of the window
+        longest = 0
+        max_freq = 0
+    
+        for right in range(len(s)):
+            hmap[s[right]] = hmap.get(s[right], 0) + 1
+            max_freq = max(max_freq, hmap[s[right]])
+
+            # Number of replacements, k is greater than
+            # the number of replacement allowed, therefore we shift
+            # the left boundary
+            while (right - left + 1) - max_freq > k:
+                hmap[s[left]] -= 1
+                if hmap[s[left]] == 0:
+                    hmap.pop(s[left])
+                left += 1   
+
+            # Updating the return value
+            longest = max(longest, right - left + 1)
+          
+        return longest
